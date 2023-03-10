94e86395c8702a3ed8f3aee8619faec8a9fe2680
nguyen18 <taylorjhnguyen@gmail.com>
2023-03-10 03:00:32 -0800

    header component

--- react_app/the-block/src/components/header/header.jsx
+++ react_app/the-block/src/components/header/header.jsx
@@ -1,2 +1,10 @@
 import React from "react";
 import './header.scss';
+
+const Header = () =>(
+    <div className='header'>
+        <h2>Go and React Chat App</h2>
+    </div>
+);
+
+export default Header;
