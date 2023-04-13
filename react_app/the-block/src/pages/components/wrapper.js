158b7e2ab741beb3bc5c34636482be1e264850b5
nguyen18 <taylorjhnguyen@gmail.com>
2023-04-13 14:54:31 -0700

    clear working tree

--- react_app/the-block/src/pages/components/wrapper.js
+++ react_app/the-block/src/pages/components/wrapper.js
@@ -1,37 +1,13 @@
 import styled from "styled-components";
+import "../../assets/fonts/FontFaces.css";
 
 export const Wrapper = styled.div`
-  font-family: "Spline Sans", sans-serif;
+  font-family: "Regular", sans-serif;
   color: white;
   font-size: 16px;
   margin: 0 auto;
   position: relative;
   height: 100%;
   overflow-x: hidden;
-
-  .spline {
-    position: absolute;
-    margin: 0;
-    top: 0;
-    right: 0;
-    width: 1200px;
-    height: 1000px;
-
-    @media (max-width: 1024px) {
-      transform: scale(0.8) translateX(200px);
-      transform-origin: top;
-    }
-    @media (max-width: 800px) {
-      transform: scale(0.7) translateX(600px);
-    }
-    @media (max-width: 600px) {
-      transform: scale(0.5) translateX(-100px);
-      right: auto;
-      left: 50%;
-      margin-left: -600px;
-    }
-    @media (max-width: 375px) {
-      transform: scale(0.45) translateX(-50px);
-    }
   }
 `;
