6a010d1f57c29dc04db311ff48e59857ad0e65a2
nguyen18 <taylorjhnguyen@gmail.com>
2023-04-18 13:29:03 -0700

    initial dashboard

--- react_app/the-block/src/pages/components/wrapper.js
+++ react_app/the-block/src/pages/components/wrapper.js
@@ -3,10 +3,11 @@ import "../../assets/fonts/FontFaces.css";
 
 export const Wrapper = styled.div`
   font-family: "Regular", sans-serif;
-  color: white;
   font-size: 16px;
   margin: 0 auto;
   position: relative;
   height: 100vh;
+  width: 100vw;
+  overflow: hidden;
   }
 `;
