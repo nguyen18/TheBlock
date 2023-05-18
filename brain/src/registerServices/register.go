0d3144fc40b5197db6425f69895e93d11ade66cf
nguyen18 <taylorjhnguyen@gmail.com>
2023-05-18 00:55:26 -0700

    logging change

--- brain/src/registerServices/register.go
+++ brain/src/registerServices/register.go
@@ -1,7 +1,7 @@
 package registerservices
 
 import (
-	"fmt"
+	"log"
 
 	grpc "google.golang.org/grpc"
 
@@ -13,7 +13,7 @@ import (
 func RegisterService(server *grpc.Server) *grpc.Server {
 	authService, err := auth.NewAuthServiceServer("postgresql://root:secret@localhost:5432/testdb?sslmode=disable")
 	if err != nil {
-		fmt.Println(err.Error())
+		log.Printf(err.Error())
 	}
 	server.RegisterService(&authpb.AuthService_ServiceDesc, authService)
 
